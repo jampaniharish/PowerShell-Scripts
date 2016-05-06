95147383c0aa5a5cae80ee26b98e3d1b0fa229b5
Matt Balzan <loopzilla@gmail.com>
2016-05-07 00:53:40 +0100

    Added files via upload

---
+++ GetApprovalRequests.ps1
@@ -0,0 +1,14 @@
+﻿
+$CMSiteServerFQDN = "" # type FQDN to your site server
+
+Enter-PSSession -ComputerName $CMSiteServerFQDN -ConfigurationName Microsoft.PowerShell32
+
+Import-Module "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1"
+
+$SMSDRV = Get-PSDrive -PSProvider CMSite
+
+CD "$($SMSDRV):"
+
+Get-CMApprovalRequest | ? {$_.CurrentState -eq 1} | select Application, LastModifiedBy, Comment
+
+Exit-PSSession
