614c5f35342aa59e5a0199573ab7afe51c3fa8f5
Matt Balzan <loopzilla@gmail.com>
2016-05-07 13:51:43 +0100

    Added files via upload

---
+++ TroubleshootPacks.ps1
@@ -0,0 +1,31 @@
+﻿$RunProgram ={
+cls
+$packs = (Get-ChildItem C:\Windows\diagnostics\system).Name
+
+$list=0
+foreach ($pack in $packs)
+
+{
+$list+=1
+write-host "[$list]"- "$pack"
+}
+
+Write-Host ""
+$choice = Read-host "Choose a Troubleshooting Pack: 1-$list"
+$sel = $packs[$choice-1]
+write-host "You have selected: $sel"
+
+
+Get-TroubleshootingPack -path C:\windows\diagnostics\system\$sel | Invoke-TroubleshootingPack
+}
+
+&$RunProgram
+
+$again = Read-Host "Back to menu?"
+    
+switch ($again)
+ {
+     "y" {&$RunProgram}
+     "n" {}
+    
+ }
